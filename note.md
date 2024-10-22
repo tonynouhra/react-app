c2ca8b1d1c2d48df01a9fd7e04bbc2342929cab2
Tony Nouhra <tony.nouhra@montymobile.com>
2024-10-22 23:06:04 +0300

    Update UI and integrate Tailwind CSS and react-icons
    Replaced BiArchive icon with BiCalendar and added new components: Search, AddAppointment, and AppointmentInfo. Loaded appointment data from data.json and displayed it in the appointment list. Updated HTML meta tags for better readability, and documented hooks usage in note.md.

--- note.md
+++ note.md
@@ -1,1 +1,6 @@
 npm install react-icons --save
+
+### Hooks
+
+- allow you to use state and other react features without having to write classes
+- 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import {BiArchive} from "react-icons/bi";
+import {BiCalendar} from "react-icons/bi";
+import Search from "./components/search";
+import AddApointment from "./components/AddApointment"
+import appointmentlist from "./data.json"
+import AppointmentInfo from "./components/Appointmentinfo";
 
 function App() {
-    return (
-        <div className="App container mx-auto mt-3 font-thin">
-            <h1 className="text-5xl"><BiArchive className="inline-block text-amber-950"/>
-                your Appointments</h1>
-        </div>
-    );
+    return (<div className="App container mx-auto mt-3 font-thin">
+        <h1 className="text-5xl"><BiCalendar className="inline-block text-amber-950"/>
+            your Appointments</h1>
+        <AddApointment/>
+        <Search/>
+        <ul class="divide-y dicide-gray-200">
+
+            {appointmentlist.map(appointment => (<AppointmentInfo key={appointment.id} appointment={appointment}/>))}
+
+
+        </ul>
+
+    </div>);
 }
 
 export default App;
+
+/*
+we are using javascript's map function to create temporary variable called appointment for each one of those items as
+they come in
+*/
