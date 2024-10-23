c3e631a644ebc596d705419d1eb64973858e112a
Tony Nouhra <tony.nouhra@montymobile.com>
2024-10-24 00:40:03 +0300

    Refactor components and fix typos
    Refactored components to improve functionality and readability, including adding `formDataPublish` and introducing state management for form data. Fixed naming inconsistencies in `src/App.js` and `src/components/AddAppointment.js`.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,79 @@
-import {useState, useEffect, useCallback} from "react";
-import {BiCalendar} from "react-icons/bi";
-import Search from "./components/search";
-import AddApointment from "./components/AddApointment"
-import AppointmentInfo from "./components/Appointmentinfo";
+import {useState, useEffect, useCallback} from 'react'
+import {BiCalendar} from "react-icons/bi"
+import Search from "./components/Search"
+import AddAppointment from "./components/AddAppointment"
+import AppointmentInfo from "./components/AppointmentInfo"
 
 function App() {
 
-    //do retrive data using fetch
     let [appointmentList, setAppointmentList] = useState([]);
     let [query, setQuery] = useState("");
-    let [sortBY, setSortBY] = useState("petName");
+    let [sortBy, setSortBy] = useState("petName");
     let [orderBy, setOrderBy] = useState("asc");
 
-    const filterAppointments = appointmentList.filter(item => {
+    const filteredAppointments = appointmentList.filter(
+        item => {
+            return (
+                item.petName.toLowerCase().includes(query.toLowerCase()) ||
+                item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+                item.aptNotes.toLowerCase().includes(query.toLowerCase())
+            )
+        }
+    ).sort((a, b) => {
+        let order = (orderBy === 'asc') ? 1 : -1;
         return (
-            item.petName.toLowerCase().includes(query.toLowerCase()) ||
-            item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
-            item.aptNotes.toLowerCase().includes(query.toLowerCase())
-
-        )//wil compare between a and b do so sorting
-    }).sort((a, b) => {
-        let order = (orderBy === "asc") ? 1 : -1;
-        return (a[sortBY].toLowerCase() < b[sortBY].toLowerCase()) ? -1 * order : 1 * order;
+            a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+                ? -1 * order : 1 * order
+        )
     })
 
     const fetchData = useCallback(() => {
         fetch('./data.json')
             .then(response => response.json())
-            .then(data => setAppointmentList(data));
-
+            .then(data => {
+                setAppointmentList(data)
+            });
     }, [])
-
 
     useEffect(() => {
         fetchData()
     }, [fetchData]);
 
-    return (<div className="App container mx-auto mt-3 font-thin">
-        <h1 className="text-5xl"><BiCalendar className="inline-block text-amber-950"/>
-            your Appointments</h1>
-        <AddApointment/>
+    return (
+        <div className="App container mx-auto mt-3 font-thin">
+            <h1 className="text-5xl mb-3">
+                <BiCalendar className="inline-block text-red-400 align-top"/>Your Appointments</h1>
+            <AddAppointment
+                onSendAppointment={myAppointment => setAppointmentList([...appointmentList, myAppointment])}
+                lastId={appointmentList.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)}
+            />
+            <Search query={query}
+                    onQueryChange={myQuery => setQuery(myQuery)}
+                    orderBy={orderBy}
+                    onOrderByChange={mySort => setOrderBy(mySort)}
+                    sortBy={sortBy}
+                    onSortByChange={mySort => setSortBy(mySort)}
+            />
 
-
-        <Search query={query} onQueryChange={myQuery => setQuery(myQuery)}
-                orderBy={orderBy}
-                onOrderByChange={mySort => setOrderBy(mySort)}
-                sortBY={sortBY}
-                onSortByChange={mySort => setSortBY(mySort)}
-        />
-
-
-        <ul class="divide-y dicide-gray-200">
-
-            {filterAppointments.map(appointment => (<AppointmentInfo key={appointment.id} appointment={appointment}
-                                                                     onDeleteAppointment={appointmentId => setAppointmentList(appointmentList.filter(appointment => appointment.id !== appointmentId)) //we are matching teh id
-                                                                     }/>))}
-
-
-        </ul>
-
-    </div>);
+            <ul className="divide-y divide-gray-200">
+                {filteredAppointments
+                    .map(appointment => (
+                        <AppointmentInfo key={appointment.id}
+                                         appointment={appointment}
+                                         onDeleteAppointment={
+                                             appointmentId =>
+                                                 setAppointmentList(appointmentList.filter(appointment =>
+                                                     appointment.id !== appointmentId))
+                                         }
+                        />
+                    ))
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
-
 /*
 we are using javascript's map function to create temporary variable called appointment for each one of those items as
 they come in
