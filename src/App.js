793cae3b55533874264608015cb09761c1f62f83
Tony Nouhra <tony.nouhra@montymobile.com>
2024-10-23 23:16:46 +0300

    Fetch appointment data and update state
    Replaced static appointment data with dynamic fetching from JSON file using `fetch` within a `useEffect` and `useCallback`. This ensures the appointment list is updated whenever the component mounts.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import {useState, useEffect, useCallback} from "react";
 import {BiCalendar} from "react-icons/bi";
 import Search from "./components/search";
 import AddApointment from "./components/AddApointment"
-import appointmentlist from "./data.json"
 import AppointmentInfo from "./components/Appointmentinfo";
 
 function App() {
+
+    //do retrive data using fetch
+    let [appointmentList, setAppointmentList] = useState([]);
+    const fetchData = useCallback(() => {
+        fetch('./data.json')
+            .then(response => response.json())
+            .then(data => setAppointmentList(data));
+
+    }, [])
+
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData]);
+
     return (<div className="App container mx-auto mt-3 font-thin">
         <h1 className="text-5xl"><BiCalendar className="inline-block text-amber-950"/>
             your Appointments</h1>
@@ -12,7 +27,7 @@ function App() {
         <Search/>
         <ul class="divide-y dicide-gray-200">
 
-            {appointmentlist.map(appointment => (<AppointmentInfo key={appointment.id} appointment={appointment}/>))}
+            {appointmentList.map(appointment => (<AppointmentInfo key={appointment.id} appointment={appointment}/>))}
 
 
         </ul>
@@ -26,3 +41,6 @@ export default App;
 we are using javascript's map function to create temporary variable called appointment for each one of those items as
 they come in
 */
+/*
+* UseEffect is if the component did mount,component did update and component will amount
+* */
